e92c771f19152eeee620ab2634127cf8cf090a82
Leonardo Ramos <leonardo.ramos@sap.com>
2026-01-28 01:19:20 -0300

    king basic movements

---
+++ src/main/java/King.java
@@ -0,0 +1,13 @@
+public class King extends Piece {
+
+  public King(Color color, int initialRank, int initialFile, Board board) {
+    super(color, initialFile, initialRank, board);
+  }
+
+  @Override
+  protected boolean canMoveTo(int rank, int file) {
+    int rankDiff = Math.abs(currentRank - rank);
+    int fileDiff = Math.abs(currentFile - file);
+    return (rankDiff <= 1 && fileDiff <= 1);
+  }
+}
---
+++ src/test/java/KingTest.java
@@ -0,0 +1,38 @@
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+class KingTest extends PieceTestTemplate {
+
+  static Stream<Arguments> getTestCases() {
+    return Stream.of(
+        Arguments.of(
+            pieceSupplier(Color.WHITE, 4, 4),
+            List.of(
+                new Movement(5, 4, true),
+                new Movement(5, 5, true),
+                new Movement(4, 5, true),
+                new Movement(3, 5, true),
+                new Movement(3, 4, true),
+                new Movement(3, 3, true),
+                new Movement(4, 3, true),
+                new Movement(5, 3, true),
+                new Movement(6, 4, true),
+                new Movement(5, 3, true),
+                new Movement(4, 6, false))),
+        Arguments.of(
+            pieceSupplier(Color.BLACK, 0, 0),
+            List.of(
+                new Movement(1, 0, true),
+                new Movement(1, 1, true),
+                new Movement(0, 1, true),
+                new Movement(2, 2, false),
+                new Movement(2, 1, false))));
+  }
+
+  private static Supplier<Piece> pieceSupplier(Color color, int rank, int file) {
+    return () -> new King(color, rank, file, new Board());
+  }
+}
