a161de4c2e92e6dbfb00d7367da0580d8f0d3d12
Leonardo Ramos <leonardo.ramos@sap.com>
2026-01-27 23:38:56 -0300

    add black tests

--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -9,7 +9,7 @@ class PawnTest extends PieceTestTemplate {
   static Stream<Arguments> getTestCases() {
     return Stream.of(
         Arguments.of(pieceSupplier(Color.WHITE, 1, 0), List.of(new Movement(2, 0, true))),
-        Arguments.of(pieceSupplier(Color.WHITE, 1, 0), List.of(new Movement(3, 0, true))),
+        Arguments.of(pieceSupplier(Color.WHITE, 2, 0), List.of(new Movement(4, 0, true))),
         Arguments.of(
             pieceSupplier(Color.WHITE, 1, 0),
             List.of(new Movement(2, 0, true), new Movement(3, 0, true))),
@@ -19,7 +19,19 @@ class PawnTest extends PieceTestTemplate {
         Arguments.of(pieceSupplier(Color.WHITE, 1, 0), List.of(new Movement(4, 0, false))),
         Arguments.of(
             pieceSupplier(Color.WHITE, 1, 0),
-            List.of(new Movement(3, 0, true), new Movement(5, 0, false))));
+            List.of(new Movement(3, 0, true), new Movement(5, 0, false))),
+        Arguments.of(pieceSupplier(Color.BLACK, 6, 0), List.of(new Movement(5, 0, true))),
+        Arguments.of(pieceSupplier(Color.BLACK, 6, 0), List.of(new Movement(4, 0, true))),
+        Arguments.of(
+            pieceSupplier(Color.BLACK, 6, 0),
+            List.of(new Movement(5, 0, true), new Movement(4, 0, true))),
+        Arguments.of(pieceSupplier(Color.BLACK, 6, 0), List.of(new Movement(6, 1, false))),
+        Arguments.of(pieceSupplier(Color.BLACK, 6, 0), List.of(new Movement(6, -1, false))),
+        Arguments.of(pieceSupplier(Color.BLACK, 6, 0), List.of(new Movement(7, 0, false))),
+        Arguments.of(pieceSupplier(Color.BLACK, 6, 0), List.of(new Movement(3, 0, false))),
+        Arguments.of(
+            pieceSupplier(Color.BLACK, 6, 0),
+            List.of(new Movement(4, 0, true), new Movement(2, 0, false))));
   }
 
   private static Supplier<Piece> pieceSupplier(Color color, int initialRank, int initialFile) {
