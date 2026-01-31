c49cf99435340a0bad38127f6d978ed0234da693
Leonardo Ramos <leonardo.ramos@sap.com>
2026-01-30 23:29:40 -0300

    refactor bishop tests

--- src/test/java/BishopTest.java
+++ src/test/java/BishopTest.java
@@ -7,25 +7,31 @@ import org.junit.jupiter.params.provider.Arguments;
 class BishopTest extends PieceTestTemplate {
 
   static Stream<Arguments> getTestCases() {
-    return Stream.of(
-        Arguments.of(
-            pieceSupplier(Color.WHITE, 3, 3),
-            List.of(
-                new Movement(5, 5, true),
-                new Movement(6, 6, true),
-                new Movement(0, 0, true),
-                new Movement(1, 1, true),
-                new Movement(2, 4, false),
-                new Movement(4, 5, false))),
-        Arguments.of(
-            pieceSupplier(Color.BLACK, 2, 4),
-            List.of(
-                new Movement(5, 7, true),
-                new Movement(0, 2, true),
-                new Movement(1, 3, true),
-                new Movement(4, 6, true),
-                new Movement(3, 3, false),
-                new Movement(4, 5, false))));
+    return Stream.of(whiteBishopDiagonalMovements(), blackBishopDiagonalMovements());
+  }
+
+  private static Arguments whiteBishopDiagonalMovements() {
+    return Arguments.of(
+        pieceSupplier(Color.WHITE, 3, 3),
+        List.of(
+            new Movement(5, 5, true),
+            new Movement(6, 6, true),
+            new Movement(0, 0, true),
+            new Movement(1, 1, true),
+            new Movement(2, 4, false),
+            new Movement(4, 5, false)));
+  }
+
+  private static Arguments blackBishopDiagonalMovements() {
+    return Arguments.of(
+        pieceSupplier(Color.BLACK, 2, 4),
+        List.of(
+            new Movement(5, 7, true),
+            new Movement(0, 2, true),
+            new Movement(1, 3, true),
+            new Movement(4, 6, true),
+            new Movement(3, 3, false),
+            new Movement(4, 5, false)));
   }
 
   private static Supplier<Piece> pieceSupplier(Color color, int rank, int file) {
